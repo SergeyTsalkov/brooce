e340f51ab524b3d6c97df6660a706f1d43223c7d
Sergey Tsalkov <stsalkov@gmail.com>
2017-09-16 15:18:50 -0700

    failing to grab a needed lock does not count as a "try" for "max_tries" purposes

--- src/brooce/runnabletask/task.go
+++ src/brooce/runnabletask/task.go
@@ -30,8 +30,6 @@ type RunnableTask struct {
 }
 
 func (task *RunnableTask) Run() (exitCode int, err error) {
-	task.Tried += 1
-
 	if len(task.Command) == 0 {
 		return
 	}
@@ -63,6 +61,8 @@ func (task *RunnableTask) Run() (exitCode int, err error) {
 		return
 	}
 	defer lock.ReleaseLocks(task.Locks)
+
+	task.Tried += 1
 
 	task.StartFlushingLog()
 	defer func() {
