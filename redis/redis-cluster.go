9b4fb2f9b6f63ef22db73b11232338d7c29c1333
Jozsef Koszo <k.joe@freemail.hu>
2019-03-31 15:40:44 +0200

    Using redis scan iterator for reliable results

--- redis/redis-cluster.go
+++ redis/redis-cluster.go
@@ -59,22 +59,14 @@ func FlushList(src, dst string) (err error) {
 
 func ScanKeys(match string) (keys []string, err error) {
 	redisClient := Get()
-	cursor := uint64(0)
 
-	for {
-		var result []string
-		result, cursor, err = redisClient.Scan(cursor, match, 10000).Result()
-
-		if err != nil {
-			return
+	err = redisClient.ForEachMaster(func(client *redis.Client) error {
+		iter := client.Scan(0, match, 10000).Iterator()
+		for iter.Next() {
+				keys = append(keys, iter.Val())
 		}
-
-		keys = append(keys, result...)
-
-		if cursor == 0 {
-			break
-		}
-	}
+		return iter.Err()
+	})
 
 	return
 }
